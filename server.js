c7d8c6998f6ea3a0a4c022f047806feaf76ecd28
Ian Fletcher <ianfletcher314@gmail.com>
2021-06-10 14:19:34 -0500

    heroku not working

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const compression = require("compression");
 require("dotenv").config()
 const PORT = process.env.PORT || 3000;
 
-const app = express(); 
+const app = express();
 app.use(logger("dev"));
 
 app.use(compression());
